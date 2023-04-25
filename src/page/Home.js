22569c08028e54c801c8df464c7fe8151a98d1a7
luanvo <luan.vo2003@hcmut.edu.vn>
2023-04-25 22:24:25 +0700

    fix home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 // import SideBar from '../components/Sidebar'
 import { Layout } from "antd";
-import SlidingPicture from "./SlidingPicture";
+import SlidingPicture from './SlidingPicture';
 import { createSearchParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
-  const backgroundImageUrl =
-    "https://images.pexels.com/photos/3648850/pexels-photo-3648850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  const backgroundImageUrl = "https://images.pexels.com/photos/3648850/pexels-photo-3648850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
   const images = [
     { src: "https://wallpapercave.com/wp/wp3785670.jpg", alt: "Image 1" },
     { src: "https://images2.alphacoders.com/589/589251.jpg", alt: "Image 2" },
@@ -25,15 +24,16 @@ export default function Home() {
     maxHeight: "100%",
     border: "solid 3px white",
     position: "flex",
-    transition: "transform 1s ease-in-out",
-  };
+    transition: "transform 1s ease-in-out"
+  }
   useEffect(() => {
     const floatingImage2 = document.getElementById("cotan");
     let animation;
 
     function startAnimation() {
       animation = setInterval(function () {
-        var currentBottom = parseInt(floatingImage2.style.bottom, 10) || 0;
+        var currentBottom =
+          parseInt(floatingImage2.style.bottom, 10) || 0;
         if (currentBottom <= -floatingImage2.offsetHeight) {
           clearInterval(animation);
           floatingImage2.style.display = "none";
@@ -95,18 +95,20 @@ export default function Home() {
 
     function startAnimation() {
       animation = setInterval(function () {
-        var currentBottom = parseInt(floatingImage1.style.bottom, 10) || 0;
-        if (currentBottom >= 500) {
+        var currentBottom =
+          parseInt(floatingImage1.style.bottom, 10) || 0;
+        if (currentBottom >= 2000) {
           clearInterval(animation);
           floatingImage1.style.display = "none";
           setTimeout(function () {
             floatingImage1.style.display = "block";
-            floatingImage1.style.bottom = "-200px";
+            floatingImage1.style.bottom = "-500px";
             startAnimation(); // Start the animation again
           }, 30);
         } else {
           currentBottom += 1;
           floatingImage1.style.bottom = currentBottom + "px";
+
         }
       }, 20);
     }
@@ -115,7 +117,6 @@ export default function Home() {
       container.addEventListener("mouseenter", function () {
         this.style.transform = "scale(1.13)";
       });
-
       container.addEventListener("click", ()=>{
         navigate({
           pathname: '/film',
@@ -124,11 +125,13 @@ export default function Home() {
           })}`
         })
       })
-
       container.addEventListener("mouseleave", function () {
         this.style.transform = "scale(1)";
       });
     }
+
+
+
 
     floatingImage1.addEventListener("mouseenter", function () {
       clearInterval(animation);
@@ -141,7 +144,7 @@ export default function Home() {
     startAnimation();
   }, [navigate]);
   return (
-    <Layout style={{ height: "100vh", overflow: 'hidden' }}>
+    <Layout style={{ height: "100vh" }}>
       <section
         className="container"
         style={{
@@ -151,7 +154,7 @@ export default function Home() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          overflow: "hidden",
+          overflow: "hidden"
         }}
       >
         <div
@@ -162,45 +165,15 @@ export default function Home() {
             width: "400px",
             height: "400px",
             left: "15vw",
-            zIndex: "4",
+            zIndex: "4"
           }}
         >
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images.alphacoders.com/301/301253.jpg"
-            alt="mission"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images5.alphacoders.com/112/1125315.jpg"
-            alt="gambit"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images4.alphacoders.com/646/646889.jpg"
-            alt="godfather"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images2.alphacoders.com/817/817125.jpg"
-            alt="spiderman"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images5.alphacoders.com/118/1188416.jpg"
-            alt="spiderman"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://wallpapercave.com/wp/wp6581266.jpg"
-            alt="spiderman"
-          />
+          <img className="conain-image" style={slideImg} src="https://images.alphacoders.com/301/301253.jpg" alt="mission" />
+          <img className="conain-image" style={slideImg} src="https://images5.alphacoders.com/112/1125315.jpg" alt="gambit" />
+          <img className="conain-image" style={slideImg} src="https://images4.alphacoders.com/646/646889.jpg" alt="godfather" />
+          <img className="conain-image" style={slideImg} src="https://images2.alphacoders.com/817/817125.jpg" alt="spiderman" />
+          <img className="conain-image" style={slideImg} src="https://images5.alphacoders.com/118/1188416.jpg" alt="spiderman" />
+          <img className="conain-image" style={slideImg} src="https://wallpapercave.com/wp/wp6581266.jpg" alt="spiderman" />
         </div>
         <div
           className="conainn"
@@ -210,45 +183,15 @@ export default function Home() {
             width: "400px",
             height: "400px",
             right: "2vw",
-            zIndex: "4",
+            zIndex: "4"
           }}
         >
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images.alphacoders.com/869/869845.jpg"
-            alt="mission"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://wallpapercave.com/wp/wp383267.jpg"
-            alt="gambit"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://wallpapercave.com/wp/wp2016392.jpg"
-            alt="godfather"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images8.alphacoders.com/123/1236928.jpg"
-            alt="spiderman"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images8.alphacoders.com/104/1046291.jpg"
-            alt="godfather"
-          />
-          <img
-            className="conain-image"
-            style={slideImg}
-            src="https://images8.alphacoders.com/616/616460.jpg"
-            alt="spiderman"
-          />
+          <img className="conain-image" style={slideImg} src="https://images.alphacoders.com/869/869845.jpg" alt="mission" />
+          <img className="conain-image" style={slideImg} src="https://wallpapercave.com/wp/wp383267.jpg" alt="gambit" />
+          <img className="conain-image" style={slideImg} src="https://wallpapercave.com/wp/wp2016392.jpg" alt="godfather" />
+          <img className="conain-image" style={slideImg} src="https://images8.alphacoders.com/123/1236928.jpg" alt="spiderman" />
+          <img className="conain-image" style={slideImg} src="https://images8.alphacoders.com/104/1046291.jpg" alt="godfather" />
+          <img className="conain-image" style={slideImg} src="https://images8.alphacoders.com/616/616460.jpg" alt="spiderman" />
         </div>
         <div
           className="slideshow-container"
@@ -261,7 +204,7 @@ export default function Home() {
             borderTop: "none",
             borderRadius: "9px",
             alignItems: "center",
-            justifyContent: "center",
+            justifyContent: "center"
           }}
         >
           <SlidingPicture imageUrl="https://images8.alphacoders.com/632/632051.png" />
@@ -287,7 +230,7 @@ export default function Home() {
                     width: "100%",
                     height: "100%",
                     objectFit: "cover",
-                    borderRadius: "9px",
+                    borderRadius: "9px"
                   }}
                   src={image.src}
                   alt={image.alt}
